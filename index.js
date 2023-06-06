6e4be56e1b66c76af428addee6cab2e76e75ef69
Gaspar Saldaña Sandoval <gaspar.saldana.sandoval@gmail.com>
2023-06-06 14:30:19 +0200

    sse updates

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { nanoid } from "nanoid";
+import handleServerSentEvents from "./sse.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors());
 
 const stuff = [
   {
@@ -28,10 +29,12 @@ const stuff = [
 ];
 
 app.post("/stuff", (req, res) => {
+  console.log(req.body);
   stuff.push({
     id: nanoid(),
     ...req.body,
   });
+  console.log(req.body);
   res.json({ status: "ok" });
   sendToAllClients();
 });
@@ -41,39 +44,7 @@ app.get("/stuff", (req, res) => {
 });
 
 const clients = [];
-app.get("/sse", (req, res) => {
-  const headers = {
-    "Content-Type": "text/event-stream",
-    Connection: "keep-alive",
-    "Cache-Control": "no-cache",
-  };
-
-  res.writeHead(200, headers);
-  const data = `data: ${JSON.stringify(stuff)}\n\n`;
-  res.write(data);
-
-  const client = {
-    time: Date.now(),
-    name: req.query.name,
-  };
-  clients.push({
-    ...client,
-    res: res,
-  });
-  setInterval(() => {
-    if (clients.length > 0) {
-      res.write(`data:\n\n`);
-    }
-  }, 5000);
-  
-  req.on("close", () => {
-    console.log(`Connection closed from ${client.name}.`);
-    clients.splice(
-      clients.findIndex((c) => c.time === client.time),
-      1
-    );
-  });
-});
+app.get("/sse", handleServerSentEvents);
 
 function sendToAllClients() {
   clients.forEach((c) => c.res.write(`data: ${JSON.stringify(stuff)}\n\n`));
